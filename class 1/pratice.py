424b3b6025f491bac2ca38ff733b70746570818b
Zain Raza <zrabbas110@gmail.com>
2024-05-25 13:25:25 +0500

    python class

---
+++ class 1/pratice.py
@@ -0,0 +1,61 @@
+# print("Hello world")
+# print('Hello world')
+# how to single line comment
+"""
+multiple line comment
+multiple line comment
+multiple line comment
+multiple line comment
+multiple line comment
+multiple line comment
+multiple line comment
+multiple line comment
+multiple line comment
+
+
+"""
+# or
+# multiple line comment
+# multiple line comment
+# multiple line comment
+# multiple line comment
+# multiple line comment
+# multiple line comment
+# multiple line comment
+# multiple line comment
+# multiple line comment
+
+# print("Hello world",end="!")
+# print("Hello","World",sep=",")
+# print("Zain","Raza",sep="-")
+# print("Hello","World",end=",""\n")
+# print("Hello","World",sep="!")
+# print("Hello","World",end="!")
+# print("in the toys store,there were lots of 'action figure' to play with me")
+# print("After the storm , we jumpted in the \"big puddles\" outside")
+# print('After the storm , we jumpted in the "big puddles" outside"')
+# print("Hello",end="-")
+# print("World",end="-")
+# print("Hello")
+# print("World")
+# print("Hello","World")
+# print("Hello","world",sep=" ")
+# print("Hello","world",sep=":")
+# print("Hello","world",sep="--")
+# print("Hello","world",sep=",")
+# print("2 X 1 =2 "*1)
+# print("2 X 1 =",*1)
+# x=1
+# y="Pakistan"
+# z="i live in  pakistan "
+# weight=60.7
+# scroce= 80
+# name="danish"
+# print(x)
+# print(y)
+# print(z)
+# print(weight)
+# print(scroce)
+# print(name)
+
+
